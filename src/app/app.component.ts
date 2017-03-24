9042c5f8a3d63d284606cafa0eba22b3a83620f7
Fong Fan <fong.fan.90@gmail.com>
2017-03-24 16:36:53 -0700

    adds click handler for selecting heroes; hides detail if none are selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,11 @@ export class Hero {
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 const HEROES: Hero[] = [
