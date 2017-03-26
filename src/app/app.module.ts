ee37e2095e355bb9c4a61873101abe1e6d5e367f
Fong Fan <fong.fan.90@gmail.com>
2017-03-25 17:55:04 -0700

    fixes some import locations and spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroService }          from './hero.service';
 import { AppRoutingModule }     from './app-routing.module';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroesComponent }      from './heroes/heroes.component';
 
 @NgModule({
   imports: [
